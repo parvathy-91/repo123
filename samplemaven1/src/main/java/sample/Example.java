3fc9e6538be11bf467def21a3d6dbd2d76362c44
USER <USER@LAPTOP-QTI51FKI>
2025-01-23 14:00:38 +0530

    new

---
+++ samplemaven1/src/main/java/sample/Example.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a[]= {1,2,3,4};
+for(int i=0;i<a.length;i++)
+{
+	System.out.println(a[i]);
+}
+	}
+
+}
